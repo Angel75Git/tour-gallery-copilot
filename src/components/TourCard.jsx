ba34f45ef6e1d4fcaf761dea0ac4bdfc095610c2
Angel75Git <aduboncenteno@usf.edu>
2025-04-26 10:46:15 -0400

    Create TourCard component with remove tour functionality

---
+++ src/components/TourCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TourCard = ({ id, name, info, image, price, onRemove }) => {
+  return (
+    <div className="tour-card">
+      <img src={image} alt={name} className="tour-image" />
+      <div className="tour-details">
+        <h2>{name}</h2>
+        <h4>${price}</h4>
+        <p>{info}</p>
+        <button onClick={() => onRemove(id)}>Not Interested</button>
+      </div>
+    </div>
+  );
+};
+
+export default TourCard;
